a53e277ff2d1116ff710f9695597f12c3948e0ea
Hassan Elsayed <hassan796elsaied@gmail.com>
2025-01-29 01:21:31 +0200

    Auto-update: Version update on branch development

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define CURRENT_BRANCH "main"
-const char *development_version[] = {"version:1.2", "Date:29-01-2025", "Time:00:16:40"};
-const char *main_version[] = {"version:1.2", "Date:29-01-2025", "Time:00:16:40"};
-#define PROJECT_VERSION "1.2"
+#define CURRENT_BRANCH "development"
+const char *development_version[] = {"version:1.3", "Date:29-01-2025", "Time:01:21:16"};
+const char *main_version[] = {"version:1.3", "Date:29-01-2025", "Time:01:21:16"};
+#define PROJECT_VERSION "1.3"
 #endif
